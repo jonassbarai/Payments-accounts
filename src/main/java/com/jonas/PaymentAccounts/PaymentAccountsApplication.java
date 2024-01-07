e3ca61c7ada2833d1eba96c8389069a840cef3d5
jonassbarai <jonas_sbarai@hotmail.com>
2024-01-07 16:15:34 -0300

    Correlão Cors swagger

--- src/main/java/com/jonas/PaymentAccounts/PaymentAccountsApplication.java
+++ src/main/java/com/jonas/PaymentAccounts/PaymentAccountsApplication.java
@@ -1,11 +1,14 @@
 package com.jonas.PaymentAccounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = @Server(url = "/", description = "Default server url"))
 public class PaymentAccountsApplication {
 
 	public static void main(String[] args) {
